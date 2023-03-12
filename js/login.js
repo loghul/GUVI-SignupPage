ee70b684170315bed33dc18d199f633d2e4b59c8
loghul <loghulm@gmail.com>
2023-03-12 19:47:05 +0530

    login updated

--- js/login.js
+++ js/login.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
+  
+  alert("Started...");
     // listen for form submission
     $('#login-form').submit(function(event) {
       event.preventDefault(); // prevent default form submission
-  
+      
       // get form data
       var username = $('#username').val();
       var password = $('#password').val();
@@ -10,13 +12,16 @@ $(document).ready(function() {
       // send data to server using AJAX
       $.ajax({
         type: 'POST',
-        url: 'login.php',
+        url: './php/login.php',
         data: { username: username, password: password },
         dataType: 'json',
         success: function(response) {
           if (response.success) {
             // redirect to profile page
-            window.location.href = 'profile.php';
+            localStorage.setItem('session_key',response.session_id);            
+            //alert(response.session_id);
+            window.location.href = 'profile.html';
+
           } else {
             // display error message
             alert(response.message);
--- php/login.php
+++ php/login.php
@@ -1,40 +1,41 @@
 <?php
 // start session
 session_start();
+include("./db.php");
 
+#require_once 'vendor/autoload.php';
 // include Redis client library
-require_once 'predis/autoload.php';
+#require_once 'predis/autoload.php';
 
 // create Redis client
-$redis = new Predis\Client();
+$redis = new Redis();
+$redis->connect(REDIS_HOST, 6379);
+#$redis->auth('REDIS_PASSWORD');
 
 // retrieve username and password from POST request
 $username = $_POST['username'];
 $password = $_POST['password'];
-
-// check if username and password are valid
 // perform database query using prepared statement
-$dsn = 'mysql:host=localhost;dbname=mydatabase;charset=utf8mb4';
-$username_db = 'myusername';
-$password_db = 'mypassword';
-$options = array(
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-);
 
 try {
-    $pdo = new PDO($dsn, $username_db, $password_db, $options);
-    $stmt = $pdo->prepare('SELECT id, username FROM users WHERE username = ? AND password = ?');
-    $stmt->execute(array($username, $password));
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-} catch (PDOException $e) {
-    echo 'Error: ' . $e->getMessage();
-    exit();
-}
+
+    $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+    $stmt = $conn->prepare('SELECT id, username,password FROM users WHERE username = ?');
+    $stmt->bind_param('s', $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $user = $result->fetch_assoc();
+
 
 // check if user exists
 if (!$user) {
     // return error message as JSON
-    echo json_encode(array('success' => false, 'message' => 'Invalid username or password.'));
+    echo json_encode(array('success' => false, 'message' => 'Invalid username or password1.'));
+    exit();
+}
+
+if (!password_verify($password, $user['password'])) {
+    echo json_encode(array('success' => false, 'message' => 'Invalid username or password2.'));
     exit();
 }
 
@@ -45,3 +46,12 @@ $redis->setex('session:' . $session_id, 3600, json_encode($session_data)); // se
 
 // set session ID in browser local storage
 echo json_encode(array('success' => true, 'session_id' => $session_id));
+//echo json_encode(array('success' => true, 's1' => password_hash($password, PASSWORD_DEFAULT), 's2' =>$password));
+
+$stmt->close();
+$conn->close();
+} catch (mysqli_sql_exception $e) {    
+    echo json_encode(array('success' => false, 'message' => 'Database error !!!'));
+    exit();
+}
+
